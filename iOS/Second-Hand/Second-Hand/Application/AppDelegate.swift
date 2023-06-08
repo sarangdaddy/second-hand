c7f0c50a2b1d7992c0736ec8ac28b8a35325da45
noah0316 <dev.noah0316@gmail.com>
2023-06-08 17:54:43 +0900

    ADD: 초기 폴더 구조 세팅 (#6)
    - 위키의 폴더구조를 기반으로 하여 초기 폴더 구조를 세팅하였습니다
    - 링크: https://github.com/masters2023-2nd-project-03/second-hand/wiki/iOS-%ED%8F%B4%EB%8D%94-%EA%B5%AC%EC%A1%B0

---
+++ iOS/Second-Hand/Second-Hand/Application/AppDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  AppDelegate.swift
+//  Second-Hand
+//
+//  Created by Noah on 2023/06/08.
+//
+
+import UIKit
+
+@main
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+}
---
+++ iOS/Second-Hand/Second-Hand/Application/SceneDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  SceneDelegate.swift
+//  Second-Hand
+//
+//  Created by Noah on 2023/06/08.
+//
+
+import UIKit
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let homeViewController = HomeViewController()
+        self.window = UIWindow(windowScene: windowScene)
+        self.window?.rootViewController = homeViewController
+        self.window?.makeKeyAndVisible()
+    }
+}
