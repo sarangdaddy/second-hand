1d1364d1b562a51db318c707cf56ab0ba7adcb36
dltpwns0 <dltpwns6@naver.com>
2023-06-09 01:15:20 +0900

    FIX: CORS 정책 사용

---
+++ backend/src/main/java/team03/secondhand/configure/WebConfigure.java
@@ -0,0 +1,16 @@
+package team03.secondhand.configure;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfigure implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "HEAD", "PUT", "DELETE", "OPTIONS", "PATCH");
+    }
+}
