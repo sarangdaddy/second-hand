729cc2e58c0a523c41914ab8d80db1c3cdd34b37
wnsqhs <wnsqhs7804@gmail.com>
2023-06-09 10:41:14 +0900

    EDIT: SwiftLint rule에 맞게 수정 (#4)
    - SwiftLint line length violation 해결

--- iOS/Second-Hand/Second-Hand/Application/SceneDelegate.swift
+++ iOS/Second-Hand/Second-Hand/Application/SceneDelegate.swift
@@ -10,8 +10,12 @@ import UIKit
 final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         let homeViewController = HomeViewController()
