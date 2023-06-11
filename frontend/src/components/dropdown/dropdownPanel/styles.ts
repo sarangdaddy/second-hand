e2d1be68b20581d9cf8fcb85e536590275c0b3b8
sarang_daddy <sarang_daddy@naver.com>
2023-06-12 00:35:57 +0900

    DESIGN: 드롭다운 마지막 패널에 border-bottom 스타일 적용 제외하기 적용

--- frontend/src/components/dropdown/dropdownPanel/styles.ts
+++ frontend/src/components/dropdown/dropdownPanel/styles.ts
@@ -1,10 +1,12 @@
 import styled from 'styled-components';
 
-export const dropdownPanel = styled.div`
+export const dropdownPanel = styled.div<{ isLastPanel?: boolean }>`
   width: 240px;
   height: 45px;
   display: flex;
   align-items: center;
+  border-bottom: solid 1px rgba(179, 179, 179, 0.39);
+  ${({ isLastPanel }) => isLastPanel && `border-bottom: none;`}
 `;
 
 export const optionTitle = styled.span`
