dc1ba2c8bd77a5da57b3bc022fc0bbe747ab0832
sarang_daddy <sarang_daddy@naver.com>
2023-06-12 00:35:57 +0900

    DESIGN: 드롭다운 마지막 패널에 border-bottom 스타일 적용 제외하기 적용

--- frontend/src/components/dropdown/dropdownPanel/index.tsx
+++ frontend/src/components/dropdown/dropdownPanel/index.tsx
@@ -3,11 +3,16 @@ import * as S from './styles';
 interface DropdownPanelProps {
   option: string;
   onClick: (option: string) => void;
+  isLastPanel?: boolean;
 }
 
-const DropdownPanel: React.FC<DropdownPanelProps> = ({ option, onClick }) => {
+const DropdownPanel: React.FC<DropdownPanelProps> = ({
+  option,
+  onClick,
+  isLastPanel,
+}) => {
   return (
-    <S.dropdownPanel onClick={() => onClick(option)}>
+    <S.dropdownPanel onClick={() => onClick(option)} isLastPanel={isLastPanel}>
       <S.optionTitle>{option}</S.optionTitle>
     </S.dropdownPanel>
   );
