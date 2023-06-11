58bef632635c96b3cc4d7ce8a488301d441e870f
sarang_daddy <sarang_daddy@naver.com>
2023-06-11 16:27:20 +0900

    FEAT: Icon 컴포넌트 생성
    - props로 원하는 아이콘을 가져오기 위한 common component

---
+++ frontend/src/assets/icons/index.jsx
@@ -0,0 +1,47 @@
+import { ReactComponent as ArrowUp } from './arrowUp.svg';
+import { ReactComponent as Camera } from './camera.svg';
+import { ReactComponent as Capacity } from './capacity.svg';
+import { ReactComponent as CellularConnection } from './cellularConnection.svg';
+import { ReactComponent as ChevronDown } from './chevronDown.svg';
+import { ReactComponent as ChevronLeft } from './chevronLeft.svg';
+import { ReactComponent as CircleFill } from './circleFill.svg';
+import { ReactComponent as Ellipsis } from './ellipsis.svg';
+import { ReactComponent as Hamburger } from './hamburger.svg';
+import { ReactComponent as Heart } from './heart.svg';
+import { ReactComponent as Home } from './home.svg';
+import { ReactComponent as Keyboard } from './keyboard.svg';
+import { ReactComponent as Message } from './message.svg';
+import { ReactComponent as Newspaper } from './newspaper.svg';
+import { ReactComponent as Person } from './person.svg';
+import { ReactComponent as Plus } from './plus.svg';
+import { ReactComponent as Search } from './search.svg';
+import { ReactComponent as Slider } from './slider.svg';
+import { ReactComponent as WifiBlack } from './Wifi_black.svg';
+import { ReactComponent as WifiWhite } from './Wifi_white.svg';
+import { ReactComponent as X } from './x.svg';
+
+const iconfiles = {
+  arrowUp: ArrowUp,
+  camera: Camera,
+  capacity: Capacity,
+  cellularConnection: CellularConnection,
+  chevronDown: ChevronDown,
+  chevronLeft: ChevronLeft,
+  circleFill: CircleFill,
+  ellipsis: Ellipsis,
+  hamburger: Hamburger,
+  heart: Heart,
+  home: Home,
+  keyboard: Keyboard,
+  message: Message,
+  newspaper: Newspaper,
+  person: Person,
+  plus: Plus,
+  search: Search,
+  slider: Slider,
+  Wifi_black: WifiBlack,
+  Wifi_white: WifiWhite,
+  x: X,
+};
+
+export default iconfiles;
