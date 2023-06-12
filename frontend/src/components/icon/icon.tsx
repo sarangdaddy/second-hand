187406d72a3cca319b0ddf88b97ce49a7e537e15
sarang_daddy <sarang_daddy@naver.com>
2023-06-12 15:05:32 +0900

    DEL: 파일 네이밍 수정을 위한 파일 제거

--- frontend/src/components/icon/icon.tsx
+++ frontend/src/components/icon/icon.tsx
@@ -9,12 +9,12 @@ interface IconProps {
   fill?: string;
 }
 
-const Icon: React.FC<IconProps> = ({
+const Icon = ({
   name = 'arrowUp',
   width = '24',
   height = '24',
   fill = 'black',
-}) => {
+}: IconProps) => {
   const IconComponent = iconfiles[name];
 
   return (
