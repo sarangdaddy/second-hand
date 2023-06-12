30873bb91639d8d03298534023f1bc4f84c44ede
sarang_daddy <sarang_daddy@naver.com>
2023-06-13 00:57:57 +0900

    FEAT: tabBar에서 이동할 페이지 생성

---
+++ frontend/src/components/ChatPage/index.tsx
@@ -0,0 +1,12 @@
+import NavBarHome from '../NavBarHome';
+
+const ChatPage = () => {
+  return (
+    <>
+      <NavBarHome type="low" />
+      <h1>채팅 이당</h1>
+    </>
+  );
+};
+
+export default ChatPage;
---
+++ frontend/src/components/HomePage/index.tsx
@@ -0,0 +1,12 @@
+import NavBarHome from '../NavBarHome';
+
+const HomePage = () => {
+  return (
+    <>
+      <NavBarHome type="low" />
+      <h1>홈 이당</h1>
+    </>
+  );
+};
+
+export default HomePage;
