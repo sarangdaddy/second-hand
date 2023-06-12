3d025912e06c892adbd59920fbebb8b1cf1fbe19
sarang_daddy <sarang_daddy@naver.com>
2023-06-13 00:57:57 +0900

    FEAT: tabBar에서 이동할 페이지 생성

---
+++ frontend/src/components/SalesPage/index.tsx
@@ -0,0 +1,12 @@
+import NavBarHome from '../NavBarHome';
+
+const SalesPage = () => {
+  return (
+    <>
+      <NavBarHome type="low" />
+      <h1>판매내역 이당</h1>
+    </>
+  );
+};
+
+export default SalesPage;
