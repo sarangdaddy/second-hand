5a38c1de9dcc62adb495e2abcae464cbcd980167
sarang_daddy <sarang_daddy@naver.com>
2023-06-13 00:57:57 +0900

    FEAT: tabBar에서 이동할 페이지 생성

---
+++ frontend/src/components/AccountPage/index.tsx
@@ -0,0 +1,12 @@
+import NavBarHome from '../NavBarHome';
+
+const AccountPage = () => {
+  return (
+    <>
+      <NavBarHome type="low" />
+      <h1>니 계정 이당</h1>
+    </>
+  );
+};
+
+export default AccountPage;
--- frontend/src/components/App/index.tsx
+++ frontend/src/components/App/index.tsx
@@ -1,15 +1,46 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 import { ThemeProvider } from 'styled-components';
 
 import theme from '../../styles/theme';
 import GlobalStyles from '../../styles/GlobalStyles';
-import NavBarHome from '../NavBarHome';
+import HomePage from '../HomePage';
+import SalesPage from '../SalesPage';
+import HeartPage from '../HeartPage';
+import ChatPage from '../ChatPage';
+import AccountPage from '../AccountPage';
 
 const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <HomePage />,
+      // TODO(sarang_daddy) : 에러 페이지 만들기
+      // errorElement: <ErrorPage />,
+    },
+    {
+      path: '/sales',
+      element: <SalesPage />,
+    },
+    {
+      path: '/heart',
+      element: <HeartPage />,
+    },
+    {
+      path: '/chat',
+      element: <ChatPage />,
+    },
+    {
+      path: '/account',
+      element: <AccountPage />,
+    },
+  ]);
+
   return (
     <>
       <ThemeProvider theme={theme}>
         <GlobalStyles />
-        <NavBarHome type="low" />
+        <RouterProvider router={router} />
       </ThemeProvider>
     </>
   );
