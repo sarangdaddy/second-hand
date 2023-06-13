a0efcd32e9a19e17afc000c0c56ba485eb284f2a
sarang_daddy <sarang_daddy@naver.com>
2023-06-13 14:34:17 +0900

    FEAT: Route 경로 설정 (#50)

---
+++ frontend/src/components/ErrorPage/index.tsx
@@ -0,0 +1,12 @@
+import NavBarHome from '../NavBarHome';
+
+const ErrorPage = () => {
+  return (
+    <>
+      <NavBarHome type="low" />
+      <h1>관심 목록 이당</h1>
+    </>
+  );
+};
+
+export default ErrorPage;
