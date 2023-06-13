a7a201b45c08b4681cfa98d87ebc1c22074810bf
JeonHyoChang <jhc1426@naver.com>
2023-06-13 15:56:59 +0900

    SETTING: member 엔티티, 레포지토리 세팅

---
+++ backend/src/main/java/team03/secondhand/domain/member/Member.java
@@ -0,0 +1,35 @@
+package team03.secondhand.domain.member;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import team03.secondhand.domain.product.Product;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Table(name = "member")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long memberId;
+
+    @Column(name = "nickname")
+    private String nickname;
+
+    @Column(name = "profile_url")
+    private String profileUrl;
+
+    @Column(name = "oauth_id")
+    private String oauthId;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "member")
+    private List<Product> products = new ArrayList<>();
+
+    // Getter and Setter methods
+}
