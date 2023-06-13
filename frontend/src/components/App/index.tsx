e245472af84b6199073795c0be3c79d3eeeeed30
sarang_daddy <sarang_daddy@naver.com>
2023-06-13 15:43:31 +0900

    FEAT: TabBar를 이용한 라우터 기능 구현
    - react-router-dom v6.12 사용

--- frontend/src/components/App/index.tsx
+++ frontend/src/components/App/index.tsx
@@ -1,7 +1,13 @@
+<<<<<<< HEAD
+=======
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+>>>>>>> 822f71f (FEAT: TabBar를 이용한 라우터 기능 구현)
 import { ThemeProvider } from 'styled-components';
 
 import theme from '../../styles/theme';
 import GlobalStyles from '../../styles/GlobalStyles';
+<<<<<<< HEAD
 import NavBarHome from '../NavBarHome';
 
 const App = () => {
@@ -12,6 +18,37 @@ const App = () => {
         <NavBarHome type="low" />
       </ThemeProvider>
     </>
+=======
+import HomePage from '../HomePage';
+import SalesPage from '../SalesPage';
+import HeartPage from '../HeartPage';
+import ChatPage from '../ChatPage';
+import AccountPage from '../AccountPage';
+import ErrorPage from '../ErrorPage';
+import Layout from '../Layout';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { path: '/', element: <HomePage /> },
+      { path: '/sales', element: <SalesPage /> },
+      { path: '/heart', element: <HeartPage /> },
+      { path: '/chat', element: <ChatPage /> },
+      { path: '/account', element: <AccountPage /> },
+    ],
+  },
+]);
+
+const App = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <RouterProvider router={router} />
+    </ThemeProvider>
+>>>>>>> 822f71f (FEAT: TabBar를 이용한 라우터 기능 구현)
   );
 };
 
