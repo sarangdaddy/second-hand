56dedafadb6d4d367970544187e689376c1683f1
JeonHyoChang <jhc1426@naver.com>
2023-06-13 15:55:49 +0900

    SETTING: category 엔티티, 레포지토리 세팅

---
+++ backend/src/main/java/team03/secondhand/domain/category/CategoryRepository.java
@@ -0,0 +1,6 @@
+package team03.secondhand.domain.category;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
