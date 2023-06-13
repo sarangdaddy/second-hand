34695ec8a85da19192d3041738c04109d43e8fd7
JeonHyoChang <jhc1426@naver.com>
2023-06-13 15:57:45 +0900

    SETTING: product 엔티티, 레포지토리 세팅

---
+++ backend/src/main/java/team03/secondhand/domain/product/ProductRepository.java
@@ -0,0 +1,6 @@
+package team03.secondhand.domain.product;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
