ca20842cdbfe7103226be9424e0e63b11d77b040
JeonHyoChang <jhc1426@naver.com>
2023-06-14 00:40:13 +0900

    CHORE: schema 수정 & 기본 데이터 추가

--- backend/src/main/resources/schema.sql
+++ backend/src/main/resources/schema.sql
@@ -1,8 +1,8 @@
+DROP TABLE IF EXISTS category, location, member, product, watchlist, product_img, chatroom, chat, member_location CASCADE;
+
 -- -----------------------------------------------------
 -- Table `category`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `category`;
-
 CREATE TABLE IF NOT EXISTS `category`
 (
     `category_id`      BIGINT       NOT NULL AUTO_INCREMENT,
@@ -15,8 +15,6 @@ CREATE TABLE IF NOT EXISTS `category`
 -- -----------------------------------------------------
 -- Table `location`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `location`;
-
 CREATE TABLE IF NOT EXISTS `location`
 (
     `location_id`         BIGINT       NOT NULL AUTO_INCREMENT,
@@ -29,14 +27,12 @@ CREATE TABLE IF NOT EXISTS `location`
 -- -----------------------------------------------------
 -- Table `member`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `member`;
-
 CREATE TABLE IF NOT EXISTS `member`
 (
     `member_id`   BIGINT       NOT NULL AUTO_INCREMENT,
     `nickname`    VARCHAR(60)  NOT NULL,
     `profile_url` VARCHAR(200) NOT NULL,
-    `oauth_id`    VARCHAR(45)  NOT NULL,
+    `oauth_id`    VARCHAR(100) NOT NULL,
     PRIMARY KEY (`member_id`),
     UNIQUE INDEX `nickname_UNIQUE` (`nickname` ASC) VISIBLE,
     UNIQUE INDEX `oauth_id_UNIQUE` (`oauth_id` ASC) VISIBLE
@@ -46,8 +42,6 @@ CREATE TABLE IF NOT EXISTS `member`
 -- -----------------------------------------------------
 -- Table `product`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `product`;
-
 CREATE TABLE IF NOT EXISTS `product`
 (
     `product_id`   BIGINT       NOT NULL AUTO_INCREMENT,
@@ -57,119 +51,92 @@ CREATE TABLE IF NOT EXISTS `product`
     `lookup_count` INT          NOT NULL DEFAULT 0,
     `sales_status` VARCHAR(15)  NOT NULL DEFAULT 'sale',
     `create_at`    DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `update_at`    DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `update_at`    DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     `category_id`  BIGINT       NOT NULL,
     `location_id`  BIGINT       NOT NULL,
     `member_id`    BIGINT       NOT NULL,
     PRIMARY KEY (`product_id`),
-    INDEX `fk_product_category1_idx` (`category_id` ASC) VISIBLE,
-    INDEX `fk_product_location1_idx` (`location_id` ASC) VISIBLE,
-    INDEX `fk_product_member1_idx` (`member_id` ASC) VISIBLE,
-    CONSTRAINT `fk_product_category1`
+    INDEX `fk_product_category_idx` (`category_id` ASC) VISIBLE,
+    INDEX `fk_product_location_idx` (`location_id` ASC) VISIBLE,
+    INDEX `fk_product_member_idx` (`member_id` ASC) VISIBLE,
+    CONSTRAINT `fk_product_category`
     FOREIGN KEY (`category_id`)
-    REFERENCES `category` (`category_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-    CONSTRAINT `fk_product_location1`
+    REFERENCES `category` (`category_id`),
+    CONSTRAINT `fk_product_location`
     FOREIGN KEY (`location_id`)
-    REFERENCES `location` (`location_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-    CONSTRAINT `fk_product_member1`
+    REFERENCES `location` (`location_id`),
+    CONSTRAINT `fk_product_member`
     FOREIGN KEY (`member_id`)
     REFERENCES `member` (`member_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION
     );
 
 
 -- -----------------------------------------------------
 -- Table `product_img`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `product_img`;
-
 CREATE TABLE IF NOT EXISTS `product_img`
 (
     `product_img_id` BIGINT       NOT NULL AUTO_INCREMENT,
-    `img_url`        VARCHAR(200) NOT NULL,
+    `img_url`        VARCHAR(300) NOT NULL,
     `product_id`     BIGINT       NOT NULL,
     PRIMARY KEY (`product_img_id`),
     INDEX `fk_product_img_product_idx` (`product_id` ASC) VISIBLE,
     CONSTRAINT `fk_product_img_product`
     FOREIGN KEY (`product_id`)
     REFERENCES `product` (`product_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION
     );
 
 
 -- -----------------------------------------------------
 -- Table `watchlist`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `watchlist`;
-
 CREATE TABLE IF NOT EXISTS `watchlist`
 (
     `watchlist_id` BIGINT NOT NULL AUTO_INCREMENT,
     `product_id`   BIGINT NOT NULL,
     `member_id`    BIGINT NOT NULL,
     PRIMARY KEY (`watchlist_id`),
-    INDEX `fk_product_has_member_member1_idx` (`member_id` ASC) VISIBLE,
-    INDEX `fk_product_has_member_product1_idx` (`product_id` ASC) VISIBLE,
-    CONSTRAINT `fk_product_has_member_product1`
+    INDEX `fk_watchlist_member_idx` (`member_id` ASC) VISIBLE,
+    INDEX `fk_watchlist_product_idx` (`product_id` ASC) VISIBLE,
+    CONSTRAINT `fk_watchlist_product`
     FOREIGN KEY (`product_id`)
-    REFERENCES `product` (`product_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-    CONSTRAINT `fk_product_has_member_member1`
+    REFERENCES `product` (`product_id`),
+    CONSTRAINT `fk_watchlist_member`
     FOREIGN KEY (`member_id`)
     REFERENCES `member` (`member_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION
-    )
-    ENGINE = InnoDB;
+    );
 
 
 -- -----------------------------------------------------
 -- Table `chatroom`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `chatroom`;
-
 CREATE TABLE IF NOT EXISTS `chatroom`
 (
     `chatroom_id` BIGINT   NOT NULL AUTO_INCREMENT,
     `create_at`   DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `update_at`   DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `update_at`   DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     `product_id`  BIGINT   NOT NULL,
     `seller_id`   BIGINT   NOT NULL,
     `buyer_id`    BIGINT   NOT NULL,
     PRIMARY KEY (`chatroom_id`),
-    INDEX `fk_chatroom_product1_idx` (`product_id` ASC) VISIBLE,
-    INDEX `fk_chatroom_member1_idx` (`seller_id` ASC) VISIBLE,
-    INDEX `fk_chatroom_member2_idx` (`buyer_id` ASC) VISIBLE,
-    CONSTRAINT `fk_chatroom_product1`
+    INDEX `fk_chatroom_product_idx` (`product_id` ASC) VISIBLE,
+    INDEX `fk_chatroom_seller_idx` (`seller_id` ASC) VISIBLE,
+    INDEX `fk_chatroom_buyer_idx` (`buyer_id` ASC) VISIBLE,
+    CONSTRAINT `fk_chatroom_product`
     FOREIGN KEY (`product_id`)
-    REFERENCES `product` (`product_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-    CONSTRAINT `fk_chatroom_member1`
+    REFERENCES `product` (`product_id`),
+    CONSTRAINT `fk_chatroom_seller`
     FOREIGN KEY (`seller_id`)
-    REFERENCES `member` (`member_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-    CONSTRAINT `fk_chatroom_member2`
+    REFERENCES `member` (`member_id`),
+    CONSTRAINT `fk_chatroom_buyer`
     FOREIGN KEY (`buyer_id`)
     REFERENCES `member` (`member_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION
     );
 
 
 -- -----------------------------------------------------
 -- Table `chat`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `chat`;
-
 CREATE TABLE IF NOT EXISTS `chat`
 (
     `chat_id`     BIGINT      NOT NULL AUTO_INCREMENT,
@@ -178,26 +145,20 @@ CREATE TABLE IF NOT EXISTS `chat`
     `read_status` VARCHAR(10) NOT NULL DEFAULT 'unread',
     `chatroom_id` BIGINT      NOT NULL,
     PRIMARY KEY (`chat_id`),
-    INDEX `fk_chat_chatroom1_idx` (`chatroom_id` ASC) VISIBLE,
-    INDEX `fk_chat_member1_idx` (`sender_id` ASC) VISIBLE,
-    CONSTRAINT `fk_chat_chatroom1`
+    INDEX `fk_chat_chatroom_idx` (`chatroom_id` ASC) VISIBLE,
+    INDEX `fk_chat_member_idx` (`sender_id` ASC) VISIBLE,
+    CONSTRAINT `fk_chat_chatroom`
     FOREIGN KEY (`chatroom_id`)
-    REFERENCES `chatroom` (`chatroom_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-    CONSTRAINT `fk_chat_member1`
+    REFERENCES `chatroom` (`chatroom_id`),
+    CONSTRAINT `fk_chat_member`
     FOREIGN KEY (`sender_id`)
     REFERENCES `member` (`member_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION
     );
 
 
 -- -----------------------------------------------------
 -- Table `member_location`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `member_location`;
-
 CREATE TABLE IF NOT EXISTS `member_location`
 (
     `member_location_id`   BIGINT  NOT NULL AUTO_INCREMENT,
@@ -205,16 +166,12 @@ CREATE TABLE IF NOT EXISTS `member_location`
     `location_location_id` BIGINT  NOT NULL,
     `main_location_status` TINYINT NOT NULL DEFAULT 0,
     PRIMARY KEY (`member_location_id`),
-    INDEX `fk_member_has_location_location1_idx` (`location_location_id` ASC) VISIBLE,
-    INDEX `fk_member_has_location_member1_idx` (`member_member_id` ASC) VISIBLE,
-    CONSTRAINT `fk_member_has_location_member1`
+    INDEX `fk_member_location_location_idx` (`location_location_id` ASC) VISIBLE,
+    INDEX `fk_member_location_member_idx` (`member_member_id` ASC) VISIBLE,
+    CONSTRAINT `fk_member_location_member`
     FOREIGN KEY (`member_member_id`)
-    REFERENCES `member` (`member_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-    CONSTRAINT `fk_member_has_location_location1`
+    REFERENCES `member` (`member_id`),
+    CONSTRAINT `fk_member_location_location`
     FOREIGN KEY (`location_location_id`)
     REFERENCES `location` (`location_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION
     );
