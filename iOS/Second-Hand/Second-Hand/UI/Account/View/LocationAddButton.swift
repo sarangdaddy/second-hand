1543eca76d122a5b1678573565d1119e12060898
wnsqhs <wnsqhs7804@gmail.com>
2023-06-15 14:55:21 +0900

    ADD: LocationAddButton 파일 생성 (#44)
    - Location을 선택할 수 있는 Button에 대한 파일을 생성하였습니다
    - Button에 대한 속성을 설정하였습니다
    - Button의 Constraint를 설정하였습니다

---
+++ iOS/Second-Hand/Second-Hand/UI/Account/View/LocationAddButton.swift
@@ -0,0 +1,65 @@
+//
+//  locationAddButton.swift
+//  Second-Hand
+//
+//  Created by PJB on 2023/06/14.
+//
+
+import UIKit
+
+class LocationAddButton: UIButton {
+    private let plusIamge = UIImage(systemName: "plus")
+    private let buttonTitle = "위치 추가"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.setupLocationButtonConfiguration()
+        self.setupButtonLayoutContraint()
+        self.setupLocationAddButtonLayer()
+        self.setupLocationAddButtonContent()
+        self.setupLocationAddButtonAppearance()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        self.setupLocationButtonConfiguration()
+        self.setupButtonLayoutContraint()
+        self.setupLocationAddButtonLayer()
+        self.setupLocationAddButtonContent()
+        self.setupLocationAddButtonAppearance()
+    }
+}
+
+extension LocationAddButton {
+    private func setupLocationButtonConfiguration() {
+        let imagePadding: CGFloat = 4
+        self.configuration = .plain()
+        self.configuration?.imagePadding = imagePadding
+    }
+    
+    private func setupLocationAddButtonLayer() {
+        self.layer.borderColor = ColorPalette.gray500?.cgColor
+        self.layer.borderWidth = 1
+        self.layer.cornerRadius = 8
+        self.clipsToBounds = true
+    }
+    
+    private func setupLocationAddButtonContent() {
+        self.setTitle(self.buttonTitle, for: .normal)
+        self.setImage(self.plusIamge, for: .normal)
+    }
+    
+    private func setupLocationAddButtonAppearance() {
+        self.titleLabel?.font = FontStyle.subHead
+        self.tintColor = ColorPalette.black
+    }
+    
+    private func setupButtonLayoutContraint() {
+        let height: CGFloat = 52
+        NSLayoutConstraint.activate(
+            [
+                self.heightAnchor.constraint(equalToConstant: height)
+            ]
+        )
+    }
+}
