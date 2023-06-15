de82e7357c633c244dead3d9088fa7863103fb41
sarang_daddy <sarang_daddy@naver.com>
2023-06-16 01:13:44 +0900

    DESIGN: CatalogPage UI 구현
    - API 나오면 데이터 받아와서 렌더링 하는 방법으로 수정 필요

--- frontend/src/components/CatalogPage/styles.ts
+++ frontend/src/components/CatalogPage/styles.ts
@@ -2,11 +2,25 @@ import styled from 'styled-components';
 
 export const Catalog = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
-  grid-gap: 10px;
+  grid-template-columns: repeat(auto-fit, minmax(80px, 1fr));
+  grid-gap: 36px;
+  padding: 40px;
 `;
 
 export const Item = styled.div`
-  background-color: lightgray;
-  padding: 10px;
+  width: 80px;
+  height: 68px;
+  background-color: skyblue;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+
+  span {
+    font-weight: 400;
+    font-size: 13px;
+    line-height: 20px;
+    margin-top: 4px;
+    color: #3c3c43;
+  }
 `;
