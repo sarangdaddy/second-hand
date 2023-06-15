a2f4e2a172aeec32112fa848a3ac1fb0aa01b7cc
sarang_daddy <sarang_daddy@naver.com>
2023-06-16 01:13:44 +0900

    DESIGN: CatalogPage UI 구현
    - API 나오면 데이터 받아와서 렌더링 하는 방법으로 수정 필요

--- frontend/src/components/CatalogPage/index.tsx
+++ frontend/src/components/CatalogPage/index.tsx
@@ -10,11 +10,6 @@ const CatalogPage = () => {
     navigate(-1);
   };
 
-  //TODO(sarang_daddy) : onClick 테스트를 위한 코드 추후 삭제
-  const checkTheMoreClick = () => {
-    console.log('완료 버튼을 클릭했습니다.');
-  };
-
   return (
     <>
       <NavBarTitle
@@ -22,21 +17,37 @@ const CatalogPage = () => {
         type="low"
         backIcon
         preTitleClick={handleBackClick}
-        rightTitleClick={checkTheMoreClick}
         centerTitle="카탈로그"
-        rightTitle="완료"
       />
-      <S.Catalog className="catalog">
-        <S.Item className="item">Item 1</S.Item>
-        <S.Item className="item">Item 2</S.Item>
-        <S.Item className="item">Item 3</S.Item>
-        <S.Item className="item">Item 4</S.Item>
-        <S.Item className="item">Item 5</S.Item>
-        <S.Item className="item">Item 6</S.Item>
-        <S.Item className="item">Item 7</S.Item>
-        <S.Item className="item">Item 8</S.Item>
-        <S.Item className="item">Item 9</S.Item>
-        <S.Item className="item">Item 10</S.Item>
+      {/*TODO(sarang_daddy) : API 나오면 수정해야함*/}
+      <S.Catalog>
+        <S.Item>
+          <img
+            src="https://second-hand-s3.s3.ap-northeast-2.amazonaws.com/category_img/category_01.PNG"
+            alt="Item 1"
+            width="44"
+            height="44"
+          />
+          <span>디지털기기</span>
+        </S.Item>
+        <S.Item>Item 2</S.Item>
+        <S.Item>Item 3</S.Item>
+        <S.Item>Item 4</S.Item>
+        <S.Item>Item 5</S.Item>
+        <S.Item>Item 6</S.Item>
+        <S.Item>Item 7</S.Item>
+        <S.Item>Item 8</S.Item>
+        <S.Item>Item 9</S.Item>
+        <S.Item>Item 10</S.Item>
+        <S.Item>Item 11</S.Item>
+        <S.Item>Item 12</S.Item>
+        <S.Item>Item 13</S.Item>
+        <S.Item>Item 14</S.Item>
+        <S.Item>Item 15</S.Item>
+        <S.Item>Item 16</S.Item>
+        <S.Item>Item 17</S.Item>
+        <S.Item>Item 18</S.Item>
+        <S.Item>Item 19</S.Item>
       </S.Catalog>
     </>
   );
