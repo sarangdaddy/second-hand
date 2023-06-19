e32f9f5acbeb076f47c88d5089b4c6196519d763
JeonHyoChang <jhc1426@naver.com>
2023-06-19 13:57:08 +0900

    EDIT: 카테고리 로직 리뷰 반영 (#72)

--- backend/src/main/java/team03/secondhand/domain/category/CategoryController.java
+++ backend/src/main/java/team03/secondhand/domain/category/CategoryController.java
@@ -2,11 +2,12 @@ package team03.secondhand.domain.category;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import team03.secondhand.domain.category.dto.response.ResponseCategoryDTO;
+import team03.secondhand.domain.DataResponse;
+import team03.secondhand.domain.StatusCode;
+import team03.secondhand.domain.category.dto.CategoryResponseDTO;
 
 import java.util.List;
 
@@ -19,8 +20,9 @@ public class CategoryController {
     private final CategoryService categoryService;
 
     @GetMapping
-    public ResponseEntity<List<ResponseCategoryDTO>> getAllCategories() {
+    public DataResponse<List<CategoryResponseDTO.Info>> getAllCategories() {
         log.debug("카테고리 목록 호출");
-        return ResponseEntity.ok(categoryService.getAllCategories());
+        List<CategoryResponseDTO.Info> categoryInfoList = categoryService.getAllCategories();
+        return new DataResponse<>(StatusCode.RESPONSE_SUCCESS, categoryInfoList);
     }
 }
--- backend/src/main/java/team03/secondhand/domain/category/CategoryService.java
+++ backend/src/main/java/team03/secondhand/domain/category/CategoryService.java
@@ -3,7 +3,7 @@ package team03.secondhand.domain.category;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import team03.secondhand.domain.category.dto.response.ResponseCategoryDTO;
+import team03.secondhand.domain.category.dto.CategoryResponseDTO;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -14,19 +14,15 @@ public class CategoryService {
 
     private final CategoryRepository categoryRepository;
 
-    @Transactional
-    public List<ResponseCategoryDTO> getAllCategories() {
+    @Transactional(readOnly = true)
+    public List<CategoryResponseDTO.Info> getAllCategories() {
         List<Category> categories = categoryRepository.findAll();
         return categories.stream()
                 .map(this::convertToDTO)
                 .collect(Collectors.toList());
     }
 
-    private ResponseCategoryDTO convertToDTO(Category category) {
-        return ResponseCategoryDTO.builder()
-                .categoryId(category.getCategoryId())
-                .title(category.getTitle())
-                .categoryImgUrl(category.getCategoryImgUrl())
-                .build();
+    private CategoryResponseDTO.Info convertToDTO(Category category) {
+        return new CategoryResponseDTO.Info(category);
     }
 }
