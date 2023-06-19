94b697c43860abc29262cb8d889bd86c51ffdc0f
sarang_daddy <sarang_daddy@naver.com>
2023-06-20 00:11:06 +0900

    FEAT: 파일 첨부 기능 추가

--- frontend/src/components/UploadPhoto/index.tsx
+++ frontend/src/components/UploadPhoto/index.tsx
@@ -1,56 +1,61 @@
-import * as S from './styles';
+import { useState, ChangeEvent, useRef } from 'react';
 
+import * as S from './styles';
 import Icon from '../Icon';
 
+interface UploadedImage {
+  id: string;
+  imageUrl: string;
+}
+
 const UploadPhoto = () => {
-  const handleDeleteBtnClink = () => {
+  const [uploadedCount, setUploadedCount] = useState<number>(0);
+  const [uploadedImages, setUploadedImages] = useState<UploadedImage[]>([]);
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+  const handleDeleteBtnClick = () => {
     console.log('이미지를 삭제합니다.');
+  };
+
+  const handleUploadImage = (event: ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      const imageUrl = URL.createObjectURL(file);
+      const newUploadedImage: UploadedImage = {
+        id: Date.now().toString(),
+        imageUrl,
+      };
+      setUploadedImages((prevImages) => [...prevImages, newUploadedImage]);
+      setUploadedCount((prevCount) => prevCount + 1);
+    }
+  };
+
+  const handleUploadIconClick = () => {
+    fileInputRef.current?.click();
   };
 
   return (
     <S.Main>
       <S.UploadPhotoContainer>
-        <S.UploadIcon>
+        <S.UploadIcon onClick={handleUploadIconClick}>
           <Icon name="camera" />
-          <span>0 / 10</span>
+          <span>{uploadedCount} / 10</span>
+          <S.UploadInput
+            type="file"
+            accept="image/*"
+            onChange={handleUploadImage}
+            ref={fileInputRef}
+          />
         </S.UploadIcon>
         <S.Photos>
-          <S.PhotoElement>
-            <S.Photo src="https://dnvefa72aowie.cloudfront.net/origin/article/202306/c211c2306522eaa60a6dce97665df82b8e076590f2627dad6b05d6cee82c0d6f_0.webp?q=82&s=300x300&t=crop" />
-            <S.DeleteBtn onClick={handleDeleteBtnClink}>
-              <Icon name="x" width="20" height="20" fill="white" />
-            </S.DeleteBtn>
-          </S.PhotoElement>
-          <S.PhotoElement>
-            <S.Photo src="https://dnvefa72aowie.cloudfront.net/origin/article/202306/c211c2306522eaa60a6dce97665df82b8e076590f2627dad6b05d6cee82c0d6f_0.webp?q=82&s=300x300&t=crop" />
-            <S.DeleteBtn onClick={handleDeleteBtnClink}>
-              <Icon name="x" width="20" height="20" fill="white" />
-            </S.DeleteBtn>
-          </S.PhotoElement>
-          <S.PhotoElement>
-            <S.Photo src="https://dnvefa72aowie.cloudfront.net/origin/article/202306/c211c2306522eaa60a6dce97665df82b8e076590f2627dad6b05d6cee82c0d6f_0.webp?q=82&s=300x300&t=crop" />
-            <S.DeleteBtn onClick={handleDeleteBtnClink}>
-              <Icon name="x" width="20" height="20" fill="white" />
-            </S.DeleteBtn>
-          </S.PhotoElement>
-          <S.PhotoElement>
-            <S.Photo src="https://dnvefa72aowie.cloudfront.net/origin/article/202306/c211c2306522eaa60a6dce97665df82b8e076590f2627dad6b05d6cee82c0d6f_0.webp?q=82&s=300x300&t=crop" />
-            <S.DeleteBtn onClick={handleDeleteBtnClink}>
-              <Icon name="x" width="20" height="20" fill="white" />
-            </S.DeleteBtn>
-          </S.PhotoElement>
-          <S.PhotoElement>
-            <S.Photo src="https://dnvefa72aowie.cloudfront.net/origin/article/202306/c211c2306522eaa60a6dce97665df82b8e076590f2627dad6b05d6cee82c0d6f_0.webp?q=82&s=300x300&t=crop" />
-            <S.DeleteBtn onClick={handleDeleteBtnClink}>
-              <Icon name="x" width="20" height="20" fill="white" />
-            </S.DeleteBtn>
-          </S.PhotoElement>
-          <S.PhotoElement>
-            <S.Photo src="https://dnvefa72aowie.cloudfront.net/origin/article/202306/c211c2306522eaa60a6dce97665df82b8e076590f2627dad6b05d6cee82c0d6f_0.webp?q=82&s=300x300&t=crop" />
-            <S.DeleteBtn onClick={handleDeleteBtnClink}>
-              <Icon name="x" width="20" height="20" fill="white" />
-            </S.DeleteBtn>
-          </S.PhotoElement>
+          {uploadedImages.map((image) => (
+            <S.PhotoElement key={image.id}>
+              <S.Photo src={image.imageUrl} />
+              <S.DeleteBtn onClick={handleDeleteBtnClick}>
+                <Icon name="x" width="20" height="20" fill="white" />
+              </S.DeleteBtn>
+            </S.PhotoElement>
+          ))}
         </S.Photos>
       </S.UploadPhotoContainer>
     </S.Main>
