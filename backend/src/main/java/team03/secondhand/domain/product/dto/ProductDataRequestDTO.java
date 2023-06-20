0a845552d22d9a057625fccc9c641ce30416eb58
dltpwns0 <dltpwns6@naver.com>
2023-06-20 16:34:58 +0900

    FEAT: 상품 예외처리 기능 추가 (#82)

--- backend/src/main/java/team03/secondhand/domain/product/dto/ProductDataRequestDTO.java
+++ backend/src/main/java/team03/secondhand/domain/product/dto/ProductDataRequestDTO.java
@@ -2,16 +2,27 @@ package team03.secondhand.domain.product.dto;
 
 import lombok.Getter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.PositiveOrZero;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Getter
 public class ProductDataRequestDTO {
+
+    @NotBlank(message = "제목을 설정해주세요")
+    @Size(min = 1, max = 120, message = "제목의 길이는 1이상 120이하여야 합니다.")
     private String title;
     private Integer price;
+    @NotBlank(message = "글을 작성해주세요")
     private String content;
+    @PositiveOrZero(message = "카테고리 ID를 입력해주세요")
     private Long categoryId;
+    @PositiveOrZero(message = "동네 ID를 입력해주세요")
     private Long locationId;
     private Long memberId;
+    @NotBlank
+    @Size(min = 1, max = 10, message = "제품 사진은 1장부터 10장까지 저장할 수 있습니다.")
     private List<String> productImageUrls;
 }
 
