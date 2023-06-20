15604ef243e478fefcbedb5220a7e63e9ec38e0d
sarang_daddy <sarang_daddy@naver.com>
2023-06-20 23:12:42 +0900

    FEAT: 카테고리 API 연동

--- frontend/src/pages/Catalog/index.tsx
+++ frontend/src/pages/Catalog/index.tsx
@@ -2,6 +2,14 @@ import { useNavigate } from 'react-router-dom';
 
 import NavBarTitle from '../../components/NavBarTitle';
 import * as S from './styles';
+import useAsync from '../../hooks/useAsync';
+import { getCategory } from '../../api/category';
+
+interface Category {
+  categoryId: number;
+  title: string;
+  categoryImgUrl: string;
+}
 
 const CatalogPage = () => {
   const navigate = useNavigate();
@@ -9,6 +17,9 @@ const CatalogPage = () => {
   const handleBackClick = () => {
     navigate(-1);
   };
+
+  const { data } = useAsync(getCategory);
+  const categoryList = data?.data;
 
   return (
     <>
@@ -19,36 +30,16 @@ const CatalogPage = () => {
         preTitleClick={handleBackClick}
         centerTitle="카탈로그"
       />
-      {/*TODO(sarang_daddy) : API 나오면 수정해야함*/}
-      <S.Catalog>
-        <S.Item>
-          <img
-            src="https://second-hand-s3.s3.ap-northeast-2.amazonaws.com/category_img/category_01.PNG"
-            alt="Item 1"
-            width="44"
-            height="44"
-          />
-          <span>디지털기기</span>
-        </S.Item>
-        <S.Item>Item 2</S.Item>
-        <S.Item>Item 3</S.Item>
-        <S.Item>Item 4</S.Item>
-        <S.Item>Item 5</S.Item>
-        <S.Item>Item 6</S.Item>
-        <S.Item>Item 7</S.Item>
-        <S.Item>Item 8</S.Item>
-        <S.Item>Item 9</S.Item>
-        <S.Item>Item 10</S.Item>
-        <S.Item>Item 11</S.Item>
-        <S.Item>Item 12</S.Item>
-        <S.Item>Item 13</S.Item>
-        <S.Item>Item 14</S.Item>
-        <S.Item>Item 15</S.Item>
-        <S.Item>Item 16</S.Item>
-        <S.Item>Item 17</S.Item>
-        <S.Item>Item 18</S.Item>
-        <S.Item>Item 19</S.Item>
-      </S.Catalog>
+      <S.Categories>
+        {categoryList?.map((category: Category) => {
+          return (
+            <S.Category key={category.categoryId}>
+              <img src={category.categoryImgUrl} width="44" height="44" />
+              <span>{category.title}</span>
+            </S.Category>
+          );
+        })}
+      </S.Categories>
     </>
   );
 };
