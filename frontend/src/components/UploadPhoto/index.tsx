c9d24b53ae8e88c41720e661e4e26192791dd23e
sarang_daddy <sarang_daddy@naver.com>
2023-06-21 15:17:23 +0900

    FEAT: 업로드된 사진 상태관리 (#76)
    - useContext 사용
    - UploadPhoto에서 SalesItemPage로 업로드된 사진 정보 전달

--- frontend/src/components/UploadPhoto/index.tsx
+++ frontend/src/components/UploadPhoto/index.tsx
@@ -1,32 +1,52 @@
-import { useState, ChangeEvent, useRef } from 'react';
+import { useState, ChangeEvent, useRef, useContext } from 'react';
 
+import {
+  postSalesItemContext,
+  PostObjectType,
+} from '../../context/SalesItem/useContext';
 import * as S from './styles';
 import Icon from '../Icon';
 
-interface UploadedImage {
+interface UploadedImageType {
   id: string;
   imageUrl: string;
 }
 
 const UploadPhoto = () => {
   const [uploadedCount, setUploadedCount] = useState<number>(0);
-  const [uploadedImages, setUploadedImages] = useState<UploadedImage[]>([]);
+  const [uploadedImages, setUploadedImages] = useState<UploadedImageType[]>([]);
   const fileInputRef = useRef<HTMLInputElement>(null);
+  const maxImageCount = 10;
+  const { postObject, setPostObject } = useContext(postSalesItemContext);
 
-  const handleDeleteBtnClick = () => {
-    console.log('이미지를 삭제합니다.');
+  const handleDeleteBtnClick = (id: string) => {
+    setUploadedImages((prevImages) =>
+      prevImages.filter((image) => image.id !== id),
+    );
+    setUploadedCount((prevCount) => prevCount - 1);
   };
 
   const handleUploadImage = (event: ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
     if (file) {
       const imageUrl = URL.createObjectURL(file);
-      const newUploadedImage: UploadedImage = {
+      const newUploadedImage: UploadedImageType = {
         id: Date.now().toString(),
         imageUrl,
       };
       setUploadedImages((prevImages) => [...prevImages, newUploadedImage]);
       setUploadedCount((prevCount) => prevCount + 1);
+
+      // TODO(sarang_daddy) : 부모 SalesItem에게 업로드된 이미지 url 전달하기
+      setPostObject((prevPostObject: PostObjectType) => {
+        return {
+          ...prevPostObject,
+          productImageUrls: [
+            ...(prevPostObject?.productImageUrls || []),
+            imageUrl,
+          ],
+        };
+      });
     }
   };
 
@@ -39,7 +59,9 @@ const UploadPhoto = () => {
       <S.UploadPhotoContainer>
         <S.UploadIcon onClick={handleUploadIconClick}>
           <Icon name="camera" />
-          <span>{uploadedCount} / 10</span>
+          <span>
+            {uploadedCount} / {maxImageCount}
+          </span>
           <S.UploadInput
             type="file"
             accept="image/*"
@@ -51,7 +73,7 @@ const UploadPhoto = () => {
           {uploadedImages.map((image) => (
             <S.PhotoElement key={image.id}>
               <S.Photo src={image.imageUrl} />
-              <S.DeleteBtn onClick={handleDeleteBtnClick}>
+              <S.DeleteBtn onClick={() => handleDeleteBtnClick(image.id)}>
                 <Icon name="x" width="20" height="20" fill="white" />
               </S.DeleteBtn>
             </S.PhotoElement>
--- frontend/src/pages/SalesItem/index.tsx
+++ frontend/src/pages/SalesItem/index.tsx
@@ -1,5 +1,10 @@
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
+import {
+  postSalesItemContext,
+  PostObjectType,
+} from '../../context/SalesItem/useContext';
 import NavBarTitle from '../../components/NavBarTitle';
 import UploadPhoto from '../../components/UploadPhoto';
 
@@ -9,21 +14,38 @@ const SalesItemPage = () => {
   const handleBackIconClick = () => {
     navigation(-1);
   };
-  const checkTheMoreClick = () => {
-    console.log('완료 버튼을 클릭했습니다.');
+
+  const initialPostObject: PostObjectType = {
+    title: null,
+    price: null,
+    content: null,
+    categoryId: null,
+    locationId: null,
+    memberId: null,
+    productImageUrls: [],
+  };
+  const [postObject, setPostObject] = useState(initialPostObject);
+
+  console.log('POST 보낼 데이터 내용', postObject);
+
+  //TODO: POST API완성되면 수정
+  const handleUploadComplete = () => {
+    console.log('POST 요청을 보냅니다.', postObject);
   };
 
   return (
     <>
-      <NavBarTitle
-        prevTitle="닫기"
-        type="low"
-        preTitleClick={handleBackIconClick}
-        rightTitleClick={checkTheMoreClick}
-        centerTitle="내 물건 팔기"
-        rightTitle="완료"
-      />
-      <UploadPhoto />
+      <postSalesItemContext.Provider value={{ postObject, setPostObject }}>
+        <NavBarTitle
+          prevTitle="닫기"
+          type="low"
+          preTitleClick={handleBackIconClick}
+          rightTitleClick={handleUploadComplete}
+          centerTitle="내 물건 팔기"
+          rightTitle="완료"
+        />
+        <UploadPhoto />
+      </postSalesItemContext.Provider>
     </>
   );
 };
