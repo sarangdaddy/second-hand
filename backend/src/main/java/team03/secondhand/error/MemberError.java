8c02775a0193d79d994427f7f9d559c64bb9cda7
dltpwns0 <dltpwns6@naver.com>
2023-06-26 20:41:41 +0900

    MOVE: custom error 이동 (#107)

---
+++ backend/src/main/java/team03/secondhand/error/MemberError.java
@@ -0,0 +1,35 @@
+package team03.secondhand.error;
+
+import lombok.Getter;
+import lombok.Setter;
+import team03.secondhand.domain.StatusCode;
+
+@Getter
+@Setter
+public class MemberError {
+
+    @Getter
+    @Setter
+    public static class DuplicatedMember extends RuntimeException {}
+
+    @Getter
+    @Setter
+    public static class TokenExpired extends RuntimeException {}
+
+    @Getter
+    @Setter
+    public static class RequireRegistration extends RuntimeException {}
+
+    @Getter
+    @Setter
+    public static class NotFoundLocation extends RuntimeException {}
+
+    @Getter
+    @Setter
+    public static class TokenIsNull extends RuntimeException {}
+
+    @Getter
+    @Setter
+    public static class InvalidGuest extends RuntimeException {}
+
+}
--- backend/src/main/java/team03/secondhand/error/Oauth2Error.java
+++ backend/src/main/java/team03/secondhand/error/Oauth2Error.java
@@ -1,15 +1,17 @@
-package team03.secondhand.domain.oauth2.error;
+package team03.secondhand.error;
 
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import team03.secondhand.domain.oauth2.dto.Oauth2DataDto;
 
-@Getter @Setter
-public class Oauth2Error{
+@Getter
+@Setter
+public class Oauth2Error {
 
-    @Getter @Setter
-    public static class RequireRegistration extends RuntimeException{
+    @Getter
+    @Setter
+    public static class RequireRegistration extends RuntimeException {
         private Oauth2DataDto.LoginInfo loginInfo;
 
         public RequireRegistration(Oauth2DataDto.LoginInfo loginInfo) {
@@ -21,11 +23,16 @@ public class Oauth2Error{
         }
     }
 
-    @Getter @Setter
+    @Getter
+    @Setter
     public static class NotFoundPlatform extends RuntimeException {}
-    @Getter @Setter
+
+    @Getter
+    @Setter
     public static class InvalidPlatform extends RuntimeException {}
-    @Getter @Setter
+
+    @Getter
+    @Setter
     public static class TokenInvalid extends RuntimeException {}
 
 
