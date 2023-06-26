d47af4ad22e65c45e6395397aaf928e79beb2d58
sarang_daddy <sarang_daddy@naver.com>
2023-06-26 18:42:53 +0900

    FEAT: ItemDetail Page 컴포넌트 생성
    - 제품 클릭시 상세페이지로 넘어가는 router 기능 추가

---
+++ frontend/src/pages/ItemDetail/index.tsx
@@ -0,0 +1,17 @@
+import { useParams } from 'react-router-dom';
+
+const ItemDetail = () => {
+  const { productId } = useParams();
+
+  return (
+    <>
+      <div>현재 제품은 {productId} 번 입니다.</div>
+      <div className="DetailSliderPhotos">제품 사진</div>
+      <div className="DetaulSellerInfo">판매자 정보</div>
+      <div className="DetailItem">물품 내용</div>
+      <div className="DetailTapBar">관심 + 채팅</div>
+    </>
+  );
+};
+
+export default ItemDetail;
