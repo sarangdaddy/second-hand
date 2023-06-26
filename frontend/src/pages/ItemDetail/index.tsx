32e4da8483207e5db24c089040b321950b3682d8
sarang_daddy <sarang_daddy@naver.com>
2023-06-26 19:18:46 +0900

    FEAT: 상세 페이지 구현을 위한 샘플 데이터 생성

--- frontend/src/pages/ItemDetail/index.tsx
+++ frontend/src/pages/ItemDetail/index.tsx
@@ -3,6 +3,24 @@ import { useParams } from 'react-router-dom';
 const ItemDetail = () => {
   const { productsId } = useParams();
 
+  // Todo : 컴포넌트 제작을 위한 샘플 아이템 (추후 삭제)
+  const sampleItem = [
+    {
+      productId: 100,
+      title: '춘식이 키링 팝니다! (급처)',
+      salesStatus: '판매중',
+      createAt: '2023-06-22T08:35:03',
+      updatedAt: '2023-06-22T08:35:03',
+      price: 9900,
+      location: '역삼1동',
+      chatRoomCount: 2,
+      watchlistCount: 3,
+      isWatchlistChecked: false,
+      productMainImgUrl:
+        'https://second-hand-s3.s3.ap-northeast-2.amazonaws.com/product_images/product_100-1',
+    },
+  ];
+
   return (
     <>
       <div>현재 제품은 {productsId} 번 입니다.</div>
