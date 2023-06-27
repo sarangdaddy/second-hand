7cdb12e2767accf9ceb466bc96afeac3f18aa58e
dltpwns0 <dltpwns6@naver.com>
2023-06-27 23:51:03 +0900

    FEAT: 채팅방 불러오기 기능 구현 (#107)

---
+++ backend/src/main/java/team03/secondhand/domain/ChatRoom/ChatRoomRepository.java
@@ -0,0 +1,18 @@
+package team03.secondhand.domain.ChatRoom;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
+
+    @Query("SELECT cr FROM ChatRoom cr " +
+            "WHERE cr.seller.memberId = :memberId OR cr.buyer.memberId = :memberId")
+    List<ChatRoom> findByMemberId(@Param("memberId") Long memberId);
+
+    Optional<ChatRoom> findByChatroomId(Long roomId);
+
+}
