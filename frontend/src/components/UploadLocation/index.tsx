7af55bc01586ee1290aaac897004b9228cd16f3f
sarang_daddy <sarang_daddy@naver.com>
2023-06-27 15:40:37 +0900

    EDIT: 사용자 정보 가져오는 API 추가

--- frontend/src/components/UploadLocation/index.tsx
+++ frontend/src/components/UploadLocation/index.tsx
@@ -1,46 +1,15 @@
-import { useEffect } from 'react';
-
-import axios from 'axios';
-
 import { ACCESS_TOKEN } from '../../constants/login';
 import Dropdown from '../Dropdown';
 import Icon from '../Icon';
 import * as S from './styles';
-import { BASE_URL } from '../../constants/api';
+import useAsync from '../../hooks/useAsync';
+import { getMembers } from '../../api/product';
 
 const UploadLocation = () => {
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        /* 마스터 토큰?
-        const token =
-          'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwiaWF0IjoxNjg3MTAyMjc1LCJleHAiOjE2ODg5MDIyNzV9.AhaCUeK_M_Ph3dVTf4VCceB-Wk2AWp1ukYdP5G4VpCU';
-        */
-
-        const accessToken = localStorage.getItem(ACCESS_TOKEN);
-
-        const response = await axios.get(`${BASE_URL}/api/members`, {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
-        });
-
-        if (response.status === 200) {
-          console.log('GET 요청이 성공적으로 완료되었습니다.');
-        } else {
-          console.log(
-            'GET 요청이 실패하였습니다. 응답 상태 코드:',
-            response.status,
-          );
-        }
-      } catch (error) {
-        const err = error as Error;
-        console.log('GET 요청 중 오류가 발생하였습니다:', err.message);
-      }
-    };
-
-    fetchData();
-  }, []);
+  // 사용자 정보 가져오기
+  const accessToken = localStorage.getItem(ACCESS_TOKEN);
+  const { data } = useAsync(() => getMembers(accessToken));
+  console.log(data);
 
   return (
     <>
