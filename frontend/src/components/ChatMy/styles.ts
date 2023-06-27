a752f0a95bd37fbf7f4117514661a94a77e331b6
sarang_daddy <sarang_daddy@naver.com>
2023-06-28 01:55:56 +0900

    FEAT: ChatPage 컴포넌트 UI 구현 (#117)

---
+++ frontend/src/components/ChatMy/styles.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const ChatContainer = styled.div`
+  background-color: #ff9500;
+  width: auto;
+  height: auto;
+  max-width: 272px;
+  padding: 8px;
+  border-radius: 15px;
+  border-bottom-right-radius: 0px;
+`;
+
+export const Message = styled.span`
+  font-size: 17px;
+  line-height: 22px;
+  font-weight: 400;
+  color: #ffffff;
+`;
