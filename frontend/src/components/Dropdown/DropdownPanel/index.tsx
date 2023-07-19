f5f446eaaa82f144f05c40558f9dab2b7faf6509
sarang_daddy <sarang_daddy@naver.com>
2023-07-20 01:11:46 +0900

    FEAT: 홈 상단 바에서 동네를 변경하면 물품 리스트가 필터되는 기능 추가 (#140)
    - 선택한 동네를 사용자의 isMainLocation : true로 수정 요청

--- frontend/src/components/Dropdown/DropdownPanel/index.tsx
+++ frontend/src/components/Dropdown/DropdownPanel/index.tsx
@@ -2,17 +2,27 @@ import * as S from './styles';
 
 interface DropdownPanelProps {
   option: string;
-  onClick: (option: string) => void;
+  onClickNonOption?: () => Promise<void> | undefined;
+  onClickOption?: () => void | undefined;
   isLastPanel?: boolean;
 }
 
 const DropdownPanel = ({
   option,
-  onClick,
+  onClickNonOption,
+  onClickOption,
   isLastPanel,
 }: DropdownPanelProps) => {
+  const handleClick = () => {
+    if (onClickNonOption) {
+      onClickNonOption();
+    } else if (onClickOption) {
+      onClickOption();
+    }
+  };
+
   return (
-    <S.DropdownPanel onClick={() => onClick(option)} isLastPanel={isLastPanel}>
+    <S.DropdownPanel onClick={handleClick} isLastPanel={isLastPanel}>
       <S.OptionTitle isLastPanel={isLastPanel}>{option}</S.OptionTitle>
     </S.DropdownPanel>
   );
