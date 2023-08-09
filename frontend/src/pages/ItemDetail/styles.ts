99cf3ba58c3d10ec792456b4d288e65687816c75
sarang_daddy <sarang_daddy@naver.com>
2023-08-09 23:32:02 +0900

    DESIGN: 판매자 정보 다지인 수정 (#173)

--- frontend/src/pages/ItemDetail/styles.ts
+++ frontend/src/pages/ItemDetail/styles.ts
@@ -1,8 +1,29 @@
 import styled from 'styled-components';
 
 export const Main = styled.main`
-  margin-top: 50px;
+  margin-top: 16px;
   margin-bottom: 80px;
   padding: 0 16px;
   width: 100%;
+`;
+
+export const SellerInfo = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  height: 54px;
+  padding: 16px;
+  margin-bottom: 16px;
+  background-color: #fafafa;
+  border-radius: 12px;
+
+  & > div:first-child {
+    ${({ theme }) => theme.typo.subhead}
+    ${({ theme }) => theme.color.neutralText}
+  }
+
+  & > div:last-child {
+    ${({ theme }) => theme.typo.subhead}
+    ${({ theme }) => theme.color.neutralTextStrong}
+  }
 `;
