078d13aac44f596cdc968b336137d1c08acc93f2
sarang_daddy <sarang_daddy@naver.com>
2023-08-11 03:10:26 +0900

    FEAT: 여러장의 이미지가 있다면 슬라이더 적용 (#176)
    - 좌우 슬라이더는 터치가 아닌 버튼으로 구현 (웹에서 구현)
    - 이미지 장수와 현재 이미지를 알려주는 네비 아이콘 구현

--- frontend/src/components/DetailSliderPhotos/index.tsx
+++ frontend/src/components/DetailSliderPhotos/index.tsx
@@ -1,16 +1,42 @@
+import { useState } from 'react';
 import * as S from './styles';
 
 interface SecondHandItemProps {
-  imageList: string;
+  imageList: string[];
 }
 
-// TODO : 아이템 이미지 리스트 다 가져오기
-
 const DetailSliderPhotos = ({ imageList }: SecondHandItemProps) => {
+  const [imgIndex, setImgIndex] = useState<number>(0);
+  const hasNext = imgIndex < imageList.length - 1;
+  // const translateX = -imgIndex * 100; // 이미지의 위치 계산
+
+  const handleImgNext = () => {
+    hasNext ? setImgIndex(imgIndex + 1) : setImgIndex(0);
+  };
+
+  const handleImgPrev = () => {
+    imgIndex > 0
+      ? setImgIndex(imgIndex - 1)
+      : setImgIndex(imageList.length - 1);
+  };
+
   return (
     <>
       <S.SliderContainer>
-        <S.SliderImage src={imageList} />
+        {/* <S.ImageContainer translateX={translateX}> */}
+        <S.SliderImage src={imageList[imgIndex]} />
+        {/* </S.ImageContainer> */}
+        <S.SliderTack>
+          <S.Button onClick={handleImgPrev}>＜</S.Button>
+          <S.ImgNavigate>
+            {imageList.map((_, index) => (
+              <S.Dot key={index} isActive={index === imgIndex}>
+                ●
+              </S.Dot>
+            ))}
+          </S.ImgNavigate>
+          <S.Button onClick={handleImgNext}>＞</S.Button>
+        </S.SliderTack>
       </S.SliderContainer>
     </>
   );
