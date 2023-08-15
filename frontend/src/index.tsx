9e525f06bbd1cf6d0d3637b2dd56cf08ee389167
sarang_daddy <sarang_daddy@naver.com>
2023-08-16 00:02:24 +0900

    EDIT: 엄격모드 설정에서 로그인 버그 수정 (#187)
    - Effect 클린업 함수 적용하여 엄격모드로 인한 2번 마운트시 생기던 로그인 버그 수정

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './components/App';
@@ -6,4 +7,8 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />;
+  </React.StrictMode>,
+);
