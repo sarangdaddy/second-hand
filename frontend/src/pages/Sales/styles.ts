e72d8108d5265fddd38f2d9f41c3f44312b8fae7
sarang_daddy <sarang_daddy@naver.com>
2023-08-17 16:48:56 +0900

    FEAT: 내가 등록한 상품의 상태 필터링 (#189)
    - 판매중(+예약중) 상품과 판매완료 상품의 필터링 기능
    - 내역이 없을 경우 "판매 내역이 없습니다." 문구를 보여준다.

--- frontend/src/pages/Sales/styles.ts
+++ frontend/src/pages/Sales/styles.ts
@@ -4,3 +4,11 @@ export const ItemsContainer = styled.div`
   margin-top: 95px;
   padding-bottom: 80px; /* TabBar의 높이만큼 조정 */
 `;
+
+export const Empty = styled.span`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  margin-top: 100%;
+  ${({ theme }) => theme.color.neutralTextWeak}
+`;
