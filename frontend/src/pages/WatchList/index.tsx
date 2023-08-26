3fd0cff5a5b7a8f4649ccaaf2626296cb074278b
sarang_daddy <sarang_daddy@naver.com>
2023-08-27 02:27:59 +0900

    FEAT: 관심 목록 페이지 케테고리 필터 UI 구현 (#192)

--- frontend/src/pages/WatchList/index.tsx
+++ frontend/src/pages/WatchList/index.tsx
@@ -15,10 +15,25 @@ import SecondHandItem from '../../components/SecondHandItem';
 const WatchListPage = () => {
   const navigate = useNavigate();
   const accessToken = localStorage.getItem(ACCESS_TOKEN);
-  const { data } = useAsync(getCategory);
-  const categoryList = data?.data;
   const [myWatchList, setMyWatchList] = useState<Item[]>();
   const isResultEmpty: boolean = myWatchList?.length === 0;
+  const [selectedCategoryId, setSelectedCategoryId] = useState<number>(0);
+
+  const { data } = useAsync(getCategory);
+  const categoryList: Category[] = data?.data;
+  const myWatchListCategories = [
+    {
+      categoryId: 0,
+      title: '전체',
+    },
+    ...(categoryList?.filter((category) =>
+      myWatchList?.some((item) => item.categoryTitle === category.title),
+    ) || []),
+  ];
+
+  const handleCategoryChoice = (categoryId: number) => {
+    setSelectedCategoryId(categoryId);
+  };
 
   const fetchWatchListData = async () => {
     const { data: myWatchList } = await getWatchProducts(accessToken);
@@ -35,37 +50,46 @@ const WatchListPage = () => {
 
   return (
     <>
-      <NavBarTitle type="high" centerTitle="관심 목록" />
-      <div className="MyWatchListContainer">
-        <div className="CategoryFilter" />
-        {!isResultEmpty ? (
-          <S.ItemsContainer>
-            {myWatchList?.map((item: Item) => {
-              return (
-                <li
-                  key={item.productId}
-                  onClick={() => handleItemClick(item.productId)}
-                >
-                  <SecondHandItem
-                    title={item.title}
-                    updatedAt={item.updatedAt}
-                    salesStatus={item.salesStatus}
-                    price={item.price}
-                    location={item.location}
-                    chatRoomCount={item.chatRoomCount}
-                    watchListCount={item.watchListCount}
-                    isWatchlistChecked={item.isWatchlistChecked}
-                    productMainImgUrl={item.productMainImgUrl}
-                    isSetEditOption={false}
-                  />
-                </li>
-              );
-            })}
-          </S.ItemsContainer>
-        ) : (
-          <S.Empty>관심 내역이 없습니다.</S.Empty>
-        )}
-      </div>
+      <NavBarTitle type="high" centerTitle="관심 목록">
+        <S.Categories>
+          {myWatchListCategories?.map((category) => (
+            <S.Category
+              key={category.categoryId}
+              onClick={() => handleCategoryChoice(category.categoryId)}
+              isActive={selectedCategoryId === category.categoryId}
+            >
+              {category.title}
+            </S.Category>
+          ))}
+        </S.Categories>
+      </NavBarTitle>
+      {!isResultEmpty ? (
+        <S.ItemsContainer>
+          {myWatchList?.map((item: Item) => {
+            return (
+              <li
+                key={item.productId}
+                onClick={() => handleItemClick(item.productId)}
+              >
+                <SecondHandItem
+                  title={item.title}
+                  updatedAt={item.updatedAt}
+                  salesStatus={item.salesStatus}
+                  price={item.price}
+                  location={item.location}
+                  chatRoomCount={item.chatRoomCount}
+                  watchListCount={item.watchListCount}
+                  isWatchlistChecked={item.isWatchlistChecked}
+                  productMainImgUrl={item.productMainImgUrl}
+                  isSetEditOption={false}
+                />
+              </li>
+            );
+          })}
+        </S.ItemsContainer>
+      ) : (
+        <S.Empty>관심 내역이 없습니다.</S.Empty>
+      )}
     </>
   );
 };
