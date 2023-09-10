2a5695fdd510b87d68bc033a6fbc4add8c2e775a
sarang_daddy <sarang_daddy@naver.com>
2023-09-11 00:41:40 +0900

    FIX: 한글 메시지 전송 시 여러 메시지 전송되면 버그 수정 (#206)

--- frontend/src/components/ChatInputBar/index.tsx
+++ frontend/src/components/ChatInputBar/index.tsx
@@ -1,19 +1,25 @@
+import { useState } from 'react';
+
 import * as S from './styles';
 
 import Icon from '../Icon';
 interface ChatInputBarProps {
   onChange: (value: string) => void;
+  sendHandler: (value: string) => void;
 }
 
-const ChatInputBar = ({ onChange }: ChatInputBarProps) => {
+const ChatInputBar = ({ onChange, sendHandler }: ChatInputBarProps) => {
+  const [isComposing, setIsComposing] = useState(false);
+
   const handleInputKeyPress = (
     event: React.KeyboardEvent<HTMLInputElement>,
   ) => {
-    if (event.key === 'Enter') {
+    if (event.key === 'Enter' && !isComposing) {
       const inputElement = event.currentTarget;
-      const inputValue = inputElement.value;
+      const inputValue = inputElement.value.trim();
       if (inputValue) {
-        onChange(inputValue);
+        sendHandler(inputValue);
+        onChange('');
         inputElement.value = '';
       }
     }
@@ -25,7 +31,8 @@ const ChatInputBar = ({ onChange }: ChatInputBarProps) => {
     ) as HTMLInputElement;
     const inputValue = inputElement.value;
     if (inputValue) {
-      onChange(inputValue);
+      sendHandler(inputValue);
+      onChange('');
       inputElement.value = '';
     }
   };
@@ -39,6 +46,8 @@ const ChatInputBar = ({ onChange }: ChatInputBarProps) => {
               id="chatInput"
               placeholder="내용을 입력하세요."
               onKeyDown={handleInputKeyPress}
+              onCompositionStart={() => setIsComposing(true)}
+              onCompositionEnd={() => setIsComposing(false)}
             />
           </S.Left>
           <S.Right onClick={handleButtonClick}>
